44544966a6f21b8ef6a2a48b3134b017b78a0cb8
shubham kalaskar <shubham.kalaskar@maritimeresearchcenter.com>
2025-09-19 09:20:36 +0530

    updated export command

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', // 👈 new static export style
-  images: {
-    unoptimized: true, // (needed if you use Next Image in static export)
-  },
+  devIndicators: false,
 };
 
 export default nextConfig;
